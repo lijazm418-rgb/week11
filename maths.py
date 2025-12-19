3c9483858d8b38eb6e20fd88a8580b10b9a527f4
lijazm418-rgb <lijazm418@gmail.com>
2025-12-19 13:15:20 +0100

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,14 @@
+operation=("enter operation (+,-,*,/):")
+num1 =int (input ("enter the firdt number:"))
+num2 =int (input ("enter the second number :"))
+
+if operation=="+":
+    input(num1+ num2)
+elif operation=="-":
+    input(num1 -num2)
+elif operation=="*":
+    input(num1 * num2)
+else:
+    input( num1 / num2)
+
+
